1766e359308d5cc85d154030ad89881717b9326f
MRxwr <nasserhatab@gmail.com>
2023-12-04 22:43:46 +0300

    Update apiAcademy.php

--- requests/views/apiAcademy.php
+++ requests/views/apiAcademy.php
@@ -5,12 +5,12 @@ if( !isset($_GET["academy"]) || empty($_GET["academy"]) ){
 }else{
 	if( $academy = selectDB2("`id`, `imageurl`, `arTitle`, `enDetails`, `arDetails`, `tiktok`, `instagram`, `snapchat`, `youtube`, `enAlert`, `arAlert`, `sport`","academies","`hidden` = '0' AND `status` = '0' AND `enTitle` LIKE '{$_GET["academy"]}'") ){
 		$listOfSports = json_decode($academy[0]["sport"],true);
-		print_r($listOfSports);
 		$response["academy"] = $academy[0];
 		if( !empty($listOfSports) && sizeof($listOfSports) > 0  ){
 			for( $i = 0; $i < sizeof($listOfSports); $i++ ){
 				$sportDetails = selectDB2("`id`, `imageurl`, `arTitle`, `enTitle`","sports","`id` = '{$listOfSports[$i]}'");
-				array_push($response["academy"]["sport"],$sportDetails);
+				print_r($sportDetails[0]);
+				array_push($response["academy"]["sport"],$sportDetails[0]);
 			}
 		}else{
 			$response["academy"]["sport"] = array();
