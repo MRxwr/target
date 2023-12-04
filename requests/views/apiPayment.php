961f6873ca1bddcbdb7c00c6e863adba67426c01
MRxwr <nasserhatab@gmail.com>
2023-12-05 02:02:04 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -3,8 +3,8 @@ if( !isset($_POST) ){
     $response["msg"] = "Please make sure you send post data before submitting.";
 	echo outputError($response);die();
 }else{
-    $data = $_GET;
-    unset($_GET);
+    $data = $_POST;
+    unset($_POST);
     
     //checking voucher
     $numberOfTimesAvalability = false;
