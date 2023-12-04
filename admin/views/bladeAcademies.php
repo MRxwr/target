9ecbfa10ea75146e1701757de7b20e7449a6c9a9
MRxwr <nasserhatab@gmail.com>
2023-12-04 10:18:15 +0300

    Update bladeAcademies.php

--- admin/views/bladeAcademies.php
+++ admin/views/bladeAcademies.php
@@ -155,8 +155,6 @@
 				<td><?php echo $academyTitle ?></td>
 				<td class="text-nowrap">
 					<a href="?v=Subscriptions&code=<?php echo $academies[$i]["id"] ?>" class="btn btn-success"><?php echo direction("Subscriptions","الإشتراكات") ?></a>
-					<a href="?v=Branches&code=<?php echo $academies[$i]["id"] ?>" class="btn btn-info"><?php echo direction("Branches","الأفرع") ?></a>
-					<a href="?v=Days&code=<?php echo $academies[$i]["id"] ?>" class="btn btn-primary"><?php echo direction("Days","الأيام") ?></a>
 					<a href="?v=Sessions&code=<?php echo $academies[$i]["id"] ?>" class="btn btn-default"><?php echo direction("Sessions","المحاضرات") ?></a>
 					<a id="<?php echo $academies[$i]["id"] ?>" class="edit btn btn-warning" data-toggle="tooltip" data-original-title="<?php echo direction("Edit","تعديل")  ?>"> <i class="fa fa-pencil text-inverse m-r-10"></i>
 					</a>
