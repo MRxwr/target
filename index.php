7d0923b578e8c62c594e34dccde8cf40197f1981
MRxwr <nasserhatab@gmail.com>
2023-12-07 02:11:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("templates/navbar.php");
 
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("templates/views","blade{$_GET["v"]}.php") ){
-	require_once("templates/views/".searchFile("views","blade{$_GET["v"]}.php"));
+	require_once("templates/views/".searchFile("templates/views","blade{$_GET["v"]}.php"));
 }else{
 	require_once("templates/views/bladeHome.php");
 }
