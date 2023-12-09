f74196d181db4c69440d2139ad820a13ce16516c
MRxwr <nasserhatab@gmail.com>
2023-12-09 22:55:59 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -27,7 +27,7 @@ if ( isset ($_GET["error"]) )
       name="viewport"
       content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
     />
-    <title>MY ACAD - CP.</title>
+    <title>TARGET - CP.</title>
     <meta
       name="description"
       content="Droopy is a Dashboard & Admin Site Responsive Template by hencework."
