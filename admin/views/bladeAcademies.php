e1fb2aaac30621a75f2a5de743e9c9e8233ad184
MRxwr <nasserhatab@gmail.com>
2023-12-14 23:45:13 +0300

    Update bladeAcademies.php

--- admin/views/bladeAcademies.php
+++ admin/views/bladeAcademies.php
@@ -86,23 +86,25 @@
 			<input type="text" name="arAlert" class="form-control" required>
 			</div>
 
-			<div class="col-md-4">
-			<label><?php echo direction("IBAN","الأيبان") ?></label>
-			<input type="text" name="iban" class="form-control" required>
-			</div>
-
-			<div class="col-md-8">
+			<div class="col-md-12">
 			<label><?php echo direction("Terms & Policy","سياسة الأكادمية") ?></label>
 			<textarea name="terms" class="form-control" style="width:100%;height:250px" required></textarea>
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-md-6">
+			<label><?php echo direction("IBAN","الأيبان") ?></label>
+			<input type="text" name="iban" class="form-control" required>
+			</div>
+
+			<div class="col-md-6">
 			<label><?php echo direction("Logo","الشعار") ?></label>
 			<input type="file" name="imageurl" class="form-control" >
 			</div>
 
 			<div id="images" style="margin-top: 10px; display:none">
-				<div class="col-md-4">
+				<div class="col-md-6">
+				</div>
+				<div class="col-md-6">
 				<img id="logoImg" src="" style="width:250px;height:250px">
 				</div>
 			</div>
