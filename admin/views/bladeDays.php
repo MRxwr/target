1d60f2c1875e88cd3d8fa5c0f2350c60fda336cb
MRxwr <nasserhatab@gmail.com>
2023-12-14 23:22:00 +0300

    Update bladeDays.php

--- admin/views/bladeDays.php
+++ admin/views/bladeDays.php
@@ -9,6 +9,17 @@ if( $academy = selectDB2("`academyId`","branches","`id` = '{$_GET["code"]}'")){
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
+	<a class="panel-title txt-dark btn btn-warning" href="?v=Branches&code=<?php echo $academyId ?>" ><?php echo direction("Back to list of branches","العوده لقائمة الأفرع") ?></a>
+</div>
+	<div class="clearfix"></div>
+</div>
+</div>
+</div>
+
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
 	<h6 class="panel-title txt-dark"><?php echo direction("Days Details","تفاصيل الأيام") ?></h6>
 </div>
 	<div class="clearfix"></div>
