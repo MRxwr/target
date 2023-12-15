d54cab001a3e8cecca90e4c1dc0d4344a3d9dd58
MRxwr <nasserhatab@gmail.com>
2023-12-16 01:42:07 +0300

    Update bladeDetails.php

--- views/bladeDetails.php
+++ views/bladeDetails.php
@@ -223,8 +223,8 @@
                                         <?php 
                                         if( $branches = selectDB("subscriptions","`sportId` = '{$subscription[0]["sportId"]}' AND `academyId` = '{$mainAcademy[0]["id"]}' AND `hidden` = '0' AND `status` = '0'") ){
                                             for( $i = 0; $i < sizeof($branches); $i++ ){
-                                                $branch = $branches[$i];
-                                                $title = direction("{$branch["enTitle"]}","{$branch["arTitle"]}");
+                                                $branch = selectDB("branches","`id` = '{$branches[$i]["branchId"]}' AND `hidden` = '0' AND `status` = '0'");
+                                                $title = direction("{$branch[0]["enTitle"]}","{$branch[0]["arTitle"]}");
                                                 echo "<div class=\"size_radio\"> <input id=\"ba{$i}\" name=\"branch\" type=\"radio\"> <label for=\"ba{$i}\"> {$title} </label> </input> </div>";
                                             }
                                         }
