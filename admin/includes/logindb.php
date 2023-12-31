87ddc11f24def51e2eeaf0b6c08d1a18ffebc275
MRxwr <nasserhatab@gmail.com>
2024-01-01 00:54:47 +0300

    Update logindb.php

--- admin/includes/logindb.php
+++ admin/includes/logindb.php
@@ -3,11 +3,12 @@ session_start ();
 require("config.php");
 require("functions.php");
 require("translate.php");
-/*
+
 if( $employee = selectDB("employees","`email` LIKE '{$_POST["email"]}' AND `password` LIKE '".sha1($_POST["password"])."' AND `hidden` != '1' AND `status` = '0'") ){
 	$coockiecode = $employee[0]["keepMeAlive"];
 	$coockiecode = explode(',',$coockiecode);
 	$GenerateNewCC = md5(rand());
+	/*
 	if ( sizeof($coockiecode) <= 3 ){
 		$coockiecodenew = array();
 		if ( !isset ($coockiecode[2]) ) { $coockiecodenew[1] = $GenerateNewCC ; } else { $coockiecodenew[0] = $coockiecode[1]; }
@@ -20,8 +21,9 @@ if( $employee = selectDB("employees","`email` LIKE '{$_POST["email"]}' AND `pass
 		header("Location: ../index.php");
 		setcookie($cookieSession."A", $GenerateNewCC, time() + (86400*30 ), "/");die();
 	}
+	*/
 }else{
 	header("Location: ../login.php?error=p");die();
 }
-*/
+
 ?>
