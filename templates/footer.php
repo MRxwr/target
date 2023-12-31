c8523e253157bd03a6139d07161a29278c2498a9
MRxwr <nasserhatab@gmail.com>
2024-01-01 02:23:31 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -21,35 +21,22 @@
         showTab(currentTab); // Display the current tab
         
         function showTab(n) {
-            // This function will display the specified tab of the form ...
-            var x = document.getElementsByClassName("tab");
-            x[n].style.display = "block";
-            // ... and fix the Previous/Next buttons:
-            if (n == 0) {
-                document.getElementById("prevBtn").style.display = "none";
-            } else {
-                document.getElementById("prevBtn").style.display = "inline";
-            }
-            if (n == (x.length - 1)) {
-                document.getElementById("nextBtn").addEventListener("click", function(event) {
-                    event.preventDefault(); // Prevent form submission
-                    if (event.target.innerHTML === "PAY NOW") {
-                        document.getElementById("#regForm").submit();
-                    }
-                });
-                document.getElementById("nextBtn").innerHTML = "PAY NOW";
-                document.getElementById("nextBtn").setAttribute("type", "submit");
-                return false;
-            } else {
-                document.getElementById("nextBtn").innerHTML = "Next";
-                document.getElementById("nextBtn").setAttribute("type", "button");
-                document.getElementById("nextBtn").removeEventListener("click", function(event) {
-                    event.preventDefault();
-                    document.getElementById("#regForm").submit();
-                });
-            }
-            // ... and run a function that displays the correct step indicator:
-            fixStepIndicator(n);
+        // This function will display the specified tab of the form ...
+        var x = document.getElementsByClassName("tab");
+        x[n].style.display = "block";
+        // ... and fix the Previous/Next buttons:
+        if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+        } else {
+        document.getElementById("prevBtn").style.display = "inline";
+        }
+        if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "PAY NOW";
+        } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+        }
+        // ... and run a function that displays the correct step indicator:
+        fixStepIndicator(n)
         }
         
         function nextPrev(n) {
