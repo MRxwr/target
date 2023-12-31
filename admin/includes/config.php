06408bafcaa20e9cd40e1558da4eebc6e82b34a3
MRxwr <nasserhatab@gmail.com>
2024-01-01 01:13:29 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,6 +6,8 @@ $dbname = "u409066344_projectTarDB";
 $dbconnect = new MySQLi($servername,$username,$password,$dbname);
 if ( $dbconnect->connect_error ){
 	die("Connection Failed: " .$dbconnect->connect_error );
+}else{
+	die("Connection Successful");
 }
 $sql = "SET CHARACTER SET utf8";
 $dbconnect->query($sql);
