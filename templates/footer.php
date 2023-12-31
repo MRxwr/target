f7af524f89f7d0ee5b529077165d83bb0606f57f
MRxwr <nasserhatab@gmail.com>
2024-01-01 02:45:33 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -20,8 +20,10 @@
         var currentTab = 0; // Current tab is set to be the first tab (0)
         showTab(currentTab); // Display the current tab
         $(document).on("click",".nxtBtn",function(){
+            if (this.innerHTML === "PAY NOW") {
                 document.getElementById("regForm").submit();
-        })
+            }
+        });
         function showTab(n) {
         // This function will display the specified tab of the form ...
         var x = document.getElementsByClassName("tab");
