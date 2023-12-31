91c19b6bb332ee912c3f5721e323a00ceaa8c24b
MRxwr <nasserhatab@gmail.com>
2024-01-01 01:24:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require("templates/navbar.php");
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
 	if( isset($_GET["academyURL"]) && !empty($_GET["academyURL"]) && $mainAcademy = selectDB("academies","`status` = '0' AND `hidden` = '0' AND `url` = '".strtolower($_GET["academyURL"])."'") ){
+		print_r($_GET);die();
 		require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
 	}else{
 		require_once("views/bladeHome.php");
