87fd5d5db53c56490ec7eb2418bc8a2403a9d467
MRxwr <nasserhatab@gmail.com>
2024-01-06 05:51:17 +0300

    Update bladeAttendance.php

--- admin/views/bladeAttendance.php
+++ admin/views/bladeAttendance.php
@@ -62,7 +62,6 @@
 			
 			<div class="col-md-12" style="margin-top:10px">
 			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
-			<input type="hidden" name="update" value="0">
 			</div>
 		</div>
 	</form>
@@ -126,11 +125,14 @@
 </div>
 				
 				<!-- Bordered Table -->
+<?php 
+if ( isset($_POST["date"] )){
+?>
 <div class="col-sm-12">
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-<h6 class="panel-title txt-dark"><?php echo direction("List of Subscriptions","قائمة الإشتراكات") ?></h6>
+<h6 class="panel-title txt-dark"><?php echo direction("List of students","قائمة الطلاب") ?></h6>
 </div>
 <div class="clearfix"></div>
 </div>
@@ -142,59 +144,28 @@
 		<thead>
 		<tr>
 		<th>#</th>
-		<th><?php echo direction("Sport","الرياضات") ?></th>
-		<th><?php echo direction("English Title","العنوان الإنجليزي") ?></th>
-		<th><?php echo direction("Arabic Title","العنوان بالعربي") ?></th>
-		<th><?php echo direction("Days","الأيام") ?></th>
-		<th><?php echo direction("Price","السعر") ?></th>
-		<th><?php echo direction("Price after discount","السعر بعد الخصم") ?></th>
+		<th><?php echo direction("#","#") ?></th>
+		<th><?php echo direction("Name","الإسم") ?></th>
+		<th><?php echo direction("Mobile","الهاتف") ?></th>
 		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
 		</tr>
 		</thead>
 		
 		<tbody>
 		<?php 
-		$orderBy = direction("enTitle","arTitle");
-		if( $subscriptions = selectDB("subscriptions","`status` = '0' AND `academyId` LIKE '{$_GET["code"]}' ORDER BY `{$orderBy}` ASC") ){
-			for( $i = 0; $i < sizeof($subscriptions); $i++ ){
-				$genders = json_decode($subscriptions[$i]["genders"],true);;
-				$gender = selectDB("genders","`id` = '{$genders[0]}'");
-				if ( $subscriptions[$i]["hidden"] == 1 ){
-					$icon = "fa fa-eye";
-					$link = "?show={$subscriptions[$i]["id"]}";
-					$hide = direction("Show","أظهر");
-				}else{
-					$icon = "fa fa-eye-slash";
-					$link = "?hide={$subscriptions[$i]["id"]}";
-					$hide = direction("Hide","إخفاء");
-				}
-				if( $sportTitle = selectDB("sports","`id` = '{$subscriptions[$i]["sportId"]}'") ){
-					$sportTitle = direction($sportTitle[0]["enTitle"],$sportTitle[0]["arTitle"]);
-				}else{
-					$sportTitle = "";
-				}
+		$orderBy = direction("id","id");
+		if( $students = selectDB("students","`status` = '0' AND `academyId` LIKE '{$_POST["academyId"]}' AND `sportId` LIKE '{$_POST["sportId"]}' AND `branchId` LIKE '{$_POST["branchId"]}' AND `sessionId` LIKE '{$_POST["sessionId"]}' AND `dayId` LIKE '{$_POST["dayId"]}' ORDER BY `{$orderBy}` ASC") ){
+			for( $i = 0; $i < sizeof($students); $i++ ){
 				?>
 				<tr>
-				<td><?php echo $counter = $i + 1 ?></td>
-				<td><?php echo $sportTitle ?><label style="display:none" id="sport<?php echo $subscriptions[$i]["id"] ?>"><?php echo $subscriptions[$i]["sportId"] ?></label></td>
-				<td><?php echo "{$subscriptions[$i]["enTitle"]} - {$gender[0]["enTitle"]} {$gender[0]["enSubTitle"]}" ?></td>
-				<td><?php echo "{$subscriptions[$i]["arTitle"]} - {$gender[0]["arTitle"]} {$gender[0]["arSubTitle"]}" ?></td>
-				<td id="numberOfDays<?php echo $subscriptions[$i]["id"]?>" ><?php echo $subscriptions[$i]["numberOfDays"] ?></td>
-				<td id="price<?php echo $subscriptions[$i]["id"]?>" ><?php echo $subscriptions[$i]["price"] ?></td>
-				<td id="priceAfterDiscount<?php echo $subscriptions[$i]["id"]?>" ><?php echo $subscriptions[$i]["priceAfterDiscount"] ?></td>
+				<td><?php echo str_pad($i, 5, "0", STR_PAD_LEFT) ?></td>
+				<td><?php echo "{$students[$i]["fName"]} {$students[$i]["mName"]} {$students[$i]["lName"]}" ?></td>
+				<td><?php echo "{$students[$i]["mobile"]}" ?></td>
 				<td class="text-nowrap">
 					<a id="<?php echo $subscriptions[$i]["id"] ?>" class="edit btn btn-warning" data-toggle="tooltip" data-original-title="<?php echo direction("Edit","تعديل") ?>"> <i class="fa fa-pencil text-inverse m-r-10"></i>
 					</a>
 					<a href="<?php echo $link . "&v={$_GET["v"]}&code={$_GET["code"]}" ?>" class="btn btn-default" data-toggle="tooltip" data-original-title="<?php echo $hide ?>"> <i class="<?php echo $icon ?> text-inverse m-r-10"></i></a>			
 					<a href="<?php echo "?delId={$subscriptions[$i]["id"]}&v={$_GET["v"]}&code={$_GET["code"]}" ?>" class="btn btn-danger" data-toggle="tooltip" data-original-title="<?php echo direction("Delete","حذف") ?>"> <i class="fa fa-times text-inverse m-r-10"></i></a>
-					<label id="enTitle<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["enTitle"] ?></label>
-					<label id="arTitle<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["arTitle"] ?></label>
-					<label id="enDetails<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["enDetails"] ?></label>
-					<label id="arDetails<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["arDetails"] ?></label>
-					<label id="enSubTitle<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["enSubTitle"] ?></label>
-					<label id="arSubTitle<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["arSubTitle"] ?></label>
-					<label id="genders<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["genders"] ?></label>
-					<label id="branches<?php echo $subscriptions[$i]["id"] ?>" style="display:none"><?php echo $subscriptions[$i]["branches"] ?></label>
 				</td>
 				</tr>
 				<?php
@@ -210,6 +181,9 @@
 </div>
 </div>
 </div>
+<?php
+}
+?>
 	<!-- JavaScript -->
 	
 	<script>
