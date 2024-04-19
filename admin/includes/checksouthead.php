90737a87286a49a9ba436f135d4959adca81c524
MRxwr <nasserhatab@gmail.com>
2024-04-20 01:19:29 +0300

    Update checksouthead.php

--- admin/includes/checksouthead.php
+++ admin/includes/checksouthead.php
@@ -2,7 +2,7 @@
 require ("config.php");
 require ("translate.php");
 require ("functions.php");
-if ( isset ( $_COOKIE[$cookieSession."A"] ) ){
+if ( isset ($_COOKIE[$cookieSession."A"]) && !empty($_COOKIE[$cookieSession."A"]) ){
 	session_start ();
 	$svdva = $_COOKIE[$cookieSession."A"];
 	if ( $user = selectDB("employees","`keepMeAlive` LIKE '%".$svdva."%'") ){
@@ -15,7 +15,7 @@ if ( isset ( $_COOKIE[$cookieSession."A"] ) ){
 	}else{
 		header("Location: logout.php");die();
 	}
-}elseif( !isset ( $_COOKIE[$cookieSession."A"] ) ){
-	header("Location: login.php");die();
+}else{
+	header("Location: logout.php");die();
 }
 ?>
